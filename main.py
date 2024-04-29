7b08f8aeaa6af5945323880ff36adf0795f2a9c9
DENILSON APAZA <gioapazac@gmail.com>
2024-04-29 11:31:02 -0500

    uso de calculadora

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-a = 5
-b = 10
-print("hola mundo")
-print("La suma es: ", a+b)
-print("La resta es: ", a-b)
+
 print("********************************************")
-print("*********CALCULADORA*********")
+print("*************CALCULADORA********************")
 print("********************************************")
+
+a = int(input("ingrese 1er numero"))
+b = int(input("ingrese 2do numero"))
+
+print("La div es",a/b)
+
+print("La suma es",a+b)
+
+print("La resta es",a-b)
+
+print("La multi es",a*b)
